6abf6ce681d2053bd430b6872df81d0d97273dcd
A00446961 <77456089+A00446961@users.noreply.github.com>
2021-04-09 04:08:07 +0530

    Updated policy details and policy controller page

--- HealthInsurenceSystem/HealthInsurenceSystem/Models/Policy.cs
+++ HealthInsurenceSystem/HealthInsurenceSystem/Models/Policy.cs
@@ -9,7 +9,7 @@ namespace HealthInsurenceSystem.Models
     public partial class Policy
     {
         [Key]
-        public int? Pnumber { get; set; }
+        public string Ptype { get; set; }
         public int? MinAge { get; set; }
         public int? MaxAge { get; set; }
         public int? MonthPremium { get; set; }
